0d1b5b2a73e472bcc9afcbd004e8e617a3923d82
alejo <alejo@undergroundcellar.com>
2021-03-20 17:29:58 -0500

    Initial commit, Create task and resd tasks working for now

---
+++ tasks/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import *
+
+def index(request):
+    tasks = Task.objects.all()
+
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    context = {
+        'tasks': tasks,
+        'form': form,
+    }
+    return render(request, 'tasks/list.html', context)
